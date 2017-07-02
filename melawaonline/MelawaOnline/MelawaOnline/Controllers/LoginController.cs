d9e658e194682b52a55195846fc676efb0756a42
Abhinav Wadhankar <abhinav.wadhankar@gmail.com>
2017-07-02 22:58:33 +0530

    LoginController added

---
+++ melawaonline/MelawaOnline/MelawaOnline/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BusinessLayer;
+
+namespace MelawaOnline.Controllers
+{
+    [RoutePrefix("Login")]
+    public class LoginController : ApiController
+    {
+
+        // POST api/login
+        [Route("SignInUser")]
+        [HttpPost]
+        public IEnumerable<string> SignInUser([FromBody]Login login)
+        {
+            return new string[] { "signinuser", "value2" };
+        }
+
+        // POST api/login
+        [Route("SignUpUser")]
+        [HttpPost]
+        public IEnumerable<string> SignUpUser([FromBody]Login login)
+        {
+            return new string[] { "SignUpUser", "value2" };
+        }
+    }
+}

aae2c23d42cbf93ae72858f244d442648a8527f3
Akash Wadhankar <wadhankar.akash@gmail.com>
2017-07-08 13:23:49 -0400

    removing properties from UserInfoDOJO

--- melawaonline/MelawaOnline/Common/UserInfoDOJO.cs
+++ melawaonline/MelawaOnline/Common/UserInfoDOJO.cs
@@ -21,17 +21,6 @@ namespace Common
         private string expectation;
         private string inches;
 
-        public string PhoneNumber { get => phoneNumber; set => phoneNumber = value; }
-        public string FirstName { get => firstName; set => firstName = value; }
-        public string FatherName { get => fatherName; set => fatherName = value; }
-        public string LastName { get => lastName; set => lastName = value; }
-        public string DateOfBirth { get => dateOfBirth; set => dateOfBirth = value; }
-        public string Gender { get => gender; set => gender = value; }
-        public string UncleName { get => uncleName; set => uncleName = value; }
-        public string UrgencyLevel { get => urgencyLevel; set => urgencyLevel = value; }
-        public string MaritalStatus { get => maritalStatus; set => maritalStatus = value; }
-        public string Feets { get => feets; set => feets = value; }
-        public string Expectation { get => expectation; set => expectation = value; }
-        public string Inches { get => inches; set => inches = value; }
+       
     }
 }

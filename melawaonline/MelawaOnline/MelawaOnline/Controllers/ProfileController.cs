316de3ce6102430f509a449b5d105df2b06b5446
Akash Wadhankar <wadhankar.akash@gmail.com>
2017-07-08 13:10:22 -0400

    adding common and resolving errors

--- melawaonline/MelawaOnline/MelawaOnline/Controllers/ProfileController.cs
+++ melawaonline/MelawaOnline/MelawaOnline/Controllers/ProfileController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using BusinessLayer;
+using Common;
 
 namespace MelawaOnline.Controllers
 {
--- melawaonline/MelawaOnline/MelawaOnline/Controllers/SearchController.cs
+++ melawaonline/MelawaOnline/MelawaOnline/Controllers/SearchController.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using BusinessLayer;
+using Common;
 
 namespace MelawaOnline.Controllers
 {
